27f99064123cc669e725fad9c1b5e70daa189c96
ubuntu6ty9 <samted.uche@gmail.com>
2023-03-21 11:51:20 +0100

    defined initial game state

---
+++ Blackjack_py/main.py
@@ -0,0 +1,13 @@
+import random
+
+player_funds = 0
+player_cards = []
+dealer_cards = []
+
+def init():
+    global player_funds
+    global player_cards
+    global dealer_cards
+    player_funds = 0
+    player_cards = []
+    dealer_cards = []

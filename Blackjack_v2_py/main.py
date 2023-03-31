052c16c2567694d93dc82675675892c4f626d22d
ubuntu6ty9 <samted.uche@gmail.com>
2023-03-31 19:03:30 +0100

    several changes

--- Blackjack_v2_py/main.py
+++ Blackjack_v2_py/main.py
@@ -7,34 +7,6 @@ else:
     quit()
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-players = {}
-player_cards = {}
 
-pot = 0
-
-# len() works for dictionaries
-
-# function to deal cards to the players and dealer
-def deal(dict):
-    # key = ''
-    value = []
-    for i in dict:
-        # key += i
-        while len(value) < 2:
-            random_card = secrets.choice(cards)
-            value.append(random_card)
-        player_cards[i] = value
-
-
-
-# this loop collects player name and bets while length of the dictionary is less than or equal to 3.
-while len(players) <= 3:
-    name = input("\nEnter your name: ")
-    bet_amount = int(input("Enter your bet: $"))
-    players[name] = bet_amount
-    pot += bet_amount
-    if input("Are there other players? (y or n): ").lower() == 'n':
-        break
-
-if input("Type 'Y' to Deal Cards?").lower() == 'y':
-    deal(dict=players)
+blackjack_multiplier = 2.5
+win_multiplier = 2

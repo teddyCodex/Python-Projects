a0f4bf037516dfcc066742ac2bd1e544f8203a89
ubuntu6ty9 <samted.uche@gmail.com>
2023-04-11 08:59:45 +0100

    refactored some code

--- Higher_Lower_Game_py/main.py
+++ Higher_Lower_Game_py/main.py
@@ -1,17 +1,18 @@
+import random
+import art
+from game_data import data
+
+score = 0
+list_len = len(data)
+game_active = True
+
 # function to clear screen
 def clear():
     print("\033[H\033[J")
     print(art.logo)
 
-import random
-import art
-from game_data import data
-
-clear()
-
-score = 0
-# generate random info from the game data list
-list_len = len(data)
+def main():
+    clear()
 
 def random_data():
     random_number = random.randint(0, list_len - 1)
@@ -19,41 +20,38 @@ def random_data():
     followers = data[random_number]['follower_count']
     return str_format, followers, random_number
 
+def correct():
+    global score, A, B
+    score += 1
+    A = B
+    clear() 
 
-
-# def run_game()
-game_active = True
-
-A, a_followers, a_random_num = random_data()
+A = random_data()
 while game_active:
     if score != 0:
         print(f"You're right! Current Score: {score}")
-    B, b_followers, b_random_num = random_data()
-    if a_random_num == b_random_num:
-        B, b_followers, b_random_num = random_data()
-    else:
-        def correct():
-            global score, a_followers, b_followers, a_random_num, b_random_num, A, B
-            score += 1
-            A = B
-            a_followers = b_followers
-            a_random_num = b_random_num
-            clear()            
-        print(f"\nCompare A: {A}")
+    B = random_data()
+    if A[2] == B[2]:
+        B = random_data()
+    else:           
+        print(f"\nCompare A: {A[0]}")
         print(art.vs)
-        print(f"Compare B: {B}\n")
+        print(f"Compare B: {B[0]}\n")
         answer = input("Who has more IG followers? 'A' or 'B': ").lower()
         if answer == 'a':
-            if a_followers > b_followers:
+            if A[1] >= B[1]:
                 correct()
             else:
                 print(f"Sorry, that's wrong. Final score: {score}\n")
                 game_active = False
         elif answer == 'b':
-            if b_followers > a_followers:
+            if B[1] >= A[1]:
                 correct()
             else:
                 print(f"Sorry, that's wrong. Final score: {score}\n")
                 game_active = False
         else:
             print('Invalid Input')
+
+if __name__ == '__main__':
+    main()

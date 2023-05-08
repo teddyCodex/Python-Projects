0b545fa3d5d3c648ff22c38ea4efe32eae5e9805
ubuntu6ty9 <samted.uche@gmail.com>
2023-05-08 06:47:13 +0100

    changed question source to API

--- Quiz_Game_py/main.py
+++ Quiz_Game_py/main.py
@@ -2,6 +2,8 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
+print("\n")
+
 question_bank = []
 
 for item in question_data:
@@ -13,4 +15,11 @@ quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
 
-print(f"Quiz Completed!\nYour final score: {quiz.score}/{quiz.question_num}\n")
+final_score = f"{quiz.score}/{len(question_bank)}"
+
+if quiz.score == len(question_bank):
+    print("You're Amazing!!\nYou completed the quiz and got all the answers right!!\n")
+    print(f"Your final score is {final_score}\n")
+else:
+    print("You've completed the quiz")
+    print(f"Your final score is {final_score}\n")

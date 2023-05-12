eb507218e02e4ecbb9743f850f915a17f65536fe
ubuntu6ty9 <samted.uche@gmail.com>
2023-05-12 04:30:23 +0100

    convert color object to list of tuples

--- hirst-painting/main.py
+++ hirst-painting/main.py
@@ -1,10 +1,17 @@
-###This code will not work in repl.it as there is no access to the colorgram package here.###
-##We talk about this in the video tutorials##
 import colorgram
 
 rgb_colors = []
-colors = colorgram.extract('image.jpg', 30)
+
+colors = colorgram.extract("image.jpg", 30)
 for color in colors:
     rgb_colors.append(color.rgb)
 
-print(rgb_colors)
+colors_list = []
+
+for index, item in enumerate(rgb_colors):
+    r = rgb_colors[index][0]
+    g = rgb_colors[index][1]
+    b = rgb_colors[index][2]
+    colors_list.append((r, g, b))
+
+print(colors_list)

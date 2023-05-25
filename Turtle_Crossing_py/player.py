ffabe9008bea313041be9624892888801855d86e
ubuntu6ty9 <samted.uche@gmail.com>
2023-05-25 19:23:41 +0100

    added player and movement mechanics

--- Turtle_Crossing_py/player.py
+++ Turtle_Crossing_py/player.py
@@ -1,7 +1,24 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    """Player class that inherits from the Turtle class
+
+    Args:
+        Turtle (class): Python turtle class
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.setheading(90)  # change heading to face north
+        self.goto(STARTING_POSITION)
+
+    def move_up(self):
+        """Moves the turtle up by MOVE_DISTANCE"""
+        self.forward(MOVE_DISTANCE)

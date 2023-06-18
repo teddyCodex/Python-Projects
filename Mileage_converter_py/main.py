8f5bdd3364910b71ec91e8bdade4bbc98955fabe
ubuntu6ty9 <samted.uche@gmail.com>
2023-06-19 00:11:54 +0100

    window setup

---
+++ Mileage_converter_py/main.py
@@ -0,0 +1,16 @@
+# miles to km = multiply by 1.609
+
+# import the library that we will use for our GUI (Graphical User Interface)
+import tkinter as tk
+
+# window set-up
+window = tk.Tk()
+window.title('Mileage Converter')
+window.minsize(width=500, height=200)
+
+
+# 4 labels
+# 1 entry
+# 1 button
+
+window.mainloop()

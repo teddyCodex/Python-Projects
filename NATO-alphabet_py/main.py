2dc0e5072cba228406dfbf6201c40c9f1aa6384d
teddyCodex <samted.uche@gmail.com>
2023-08-15 23:35:46 +0100

    added error handling

--- NATO-alphabet_py/main.py
+++ NATO-alphabet_py/main.py
@@ -1,11 +1,12 @@
 import pandas, turtle
+import time
 
 pen = turtle.Turtle()
 pen.color("azure")
 pen.hideturtle()
 screen = turtle.Screen()
-screen.bgcolor("#110011")
-screen.setup(width=1000, height=600)
+screen.bgcolor("#333")
+screen.setup(width=800, height=600)
 
 nato_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
@@ -13,16 +14,30 @@ nato_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 # {"A": "Alfa", "B": "Bravo"}
 nato_dict = {row.letter: row.code for (index, row) in nato_data.iterrows()}
 
-word = screen.textinput(title="Convert a word", prompt="Enter a word")
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def generate_word():
+    word = screen.textinput(title="Convert a word", prompt="Enter a word")
 
-nato_word = [nato_dict[letter.upper()] for letter in word]
+    try:
+        nato_word = [nato_dict[letter.upper()] for letter in word]
+    except KeyError:
+        pen.write(
+            f"Only letters in the alphabets please.",
+            align="center",
+            font=("PT Mono", 14, "bold"),
+        )
+        time.sleep(2)
+        pen.clear()
+        generate_word()
+    else:
+        pen.write(
+            f"{word}:\n\n{nato_word}",
+            align="center",
+            font=("PT Mono", 14, "bold"),
+        )
 
-pen.write(
-    f"{word}:\n\n{nato_word}",
-    align="center",
-    font=("PT Mono", 14, "bold"),
-)
+
+generate_word()
 
 turtle.mainloop()

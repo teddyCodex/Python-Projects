811d01a3c8dfc1317eafbf523a5d016f5c150bc3
teddyCodex <samted.uche@gmail.com>
2023-09-05 23:11:48 +0100

    initialized hat class

--- probability-calculator/prob_calculator.py
+++ probability-calculator/prob_calculator.py
@@ -5,8 +5,13 @@ import random
 
 
 class Hat:
-    pass
+    def __init__(self, **kwargs) -> None:
+        self.contents = list()
+        for item in kwargs:
+            value = kwargs.get(item)
+            for _ in range(value):
+                self.contents.append(item)
 
 
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
+# def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+#     pass

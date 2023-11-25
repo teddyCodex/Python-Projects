ddc5374b446b6ae13d7e56a2c3662d7feeea1d92
ubuntu6ty9 <samted.uche@gmail.com>
2023-11-25 22:32:19 +0100

    final ui changes

--- quizzler/ui.py
+++ quizzler/ui.py
@@ -24,10 +24,10 @@ class QuizUI:
         )
         self.score_label.grid(row=0, column=1, pady=15)
 
-        self.canvas = Canvas(master=self.window, height=250, width=300)
+        self.canvas = Canvas(master=self.window, height=250, width=300, bg="#333")
         self.canvas.grid(row=1, columnspan=2, pady=20)
         self.question_text = self.canvas.create_text(
-            150, 125, text=text, font=("Arial", 20, "italic"), width=280
+            150, 125, text=text, font=("Arial", 20, "italic"), width=280, fill="white"
         )
 
         true_img = PhotoImage(file="images/true.png")
@@ -38,7 +38,7 @@ class QuizUI:
             image=true_img,
             highlightthickness=0,
             bd=0,
-            command=self.true_button,
+            command=self.true_clicked,
         )
         self.true_btn.grid(row=3, column=0, pady=50)
         self.false_btn = Button(
@@ -46,7 +46,7 @@ class QuizUI:
             image=false_img,
             highlightthickness=0,
             bd=0,
-            command=self.false_button,
+            command=self.false_clicked,
         )
         self.false_btn.grid(row=3, column=1, pady=50)
 
@@ -55,11 +55,30 @@ class QuizUI:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="#333")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(
+                self.question_text,
+                text=f"End of Quiz\nFinal Score: {self.quiz.score}/10",
+            )
+            self.true_btn.config(state="disabled")
+            self.false_btn.config(state="disabled")
 
-    def true_button(self):
-        self.quiz.check_answer("True")
+    def true_clicked(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
 
-    def false_button(self):
-        self.quiz.check_answer("False")
+    def false_clicked(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right: bool):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
